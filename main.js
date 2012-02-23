f85717a76d0037572844fec0ab0d0a4954dc0d3d
bnowel <bnowel@yahoo.com>
2012-02-22 17:54:52 -0800

    new demo

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-var app = require('express').createServer()
+var express = require('express')
+  , app = express.createServer()
   , io = require('socket.io').listen(app);
 
-app.listen(8080);
+app.configure(function(){
+      app.use(express.static(__dirname + '/static'));
+      //app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+      app.use(express.bodyParser());
+  });
+app.listen(process.env.PORT);
 
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+  res.sendfile(__dirname + '/static/simpleClient.html');
 });
 
 io.sockets.on('connection', function (socket) {
