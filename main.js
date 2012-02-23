7b76dbc67c3ad4a13f69b1295a340efb3d64ad13
bnowel <bnowel@yahoo.com>
2012-02-22 17:46:18 -0800

    added different example

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
-// This code is pulled straight from https://github.com/LearnBoost/Socket.IO
-// It's a stub for something that will work eventually.
-var port = process.env.C9_PORT || process.env.PORT || 3000;
+var app = require('express').createServer()
+  , io = require('socket.io').listen(app);
 
-var io = require('socket.io').listen(port);
+app.listen(8080);
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
 
 io.sockets.on('connection', function (socket) {
-  io.sockets.emit('this', { will: 'be received by everyone' });
-
-  socket.on('private message', function (from, msg) {
-    console.log('I received a private message by ', from, ' saying ', msg);
-  });
-
-  socket.on('disconnect', function () {
-    io.sockets.emit('user disconnected');
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
   });
 });
