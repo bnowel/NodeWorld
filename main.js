142ce9f32ab4c924ec62ff8a2a480c790909d001
bnowel <bnowel@yahoo.com>
2012-02-26 23:37:22 -0800

    Made names move around the screen

--- main.js
+++ main.js
@@ -7,20 +7,37 @@ app.configure(function(){
       //app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
       app.use(express.bodyParser());
   });
-app.listen(process.env.C9_PORT || process.env.PORT);
+
+app.listen(process.env.C9_PORT || process.env.PORT || 3000);
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/static/simpleClient.html');
 });
 
+var world = {
+    bounds: {maxX: 600, maxY: 600}
+    //, players: []
+};
+
 io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('send message', function (data) {
-    console.log(data);
-    // send message back to the client
-    socket.emit('news', { news: data });
+    socket.emit('news', { hello: 'world' });
+    socket.on('send message', function (data) {
+        console.log(data);
+        // send message back to the client
+        socket.emit('news', { news: data });
     
-    // send message out to all OTHER clients
-    socket.broadcast.emit('news', { news: data });
-  });
+        // send message out to all OTHER clients
+        socket.broadcast.emit('news', { news: data });
+    });
+    socket.on('move', function(data) {
+        data.id = socket.id;
+        socket.broadcast.emit('move', data); 
+    });
+    socket.on('name', function(data) {
+        data.id = socket.id;
+        socket.broadcast.emit('name', data); 
+    });
+    socket.on('disconnect', function(data) {
+       socket.broadcast.emit('died', {id: socket.id});
+    });
 });
