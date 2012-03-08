6e1ec2f868fd3b18f6d4926c0360c0640e06e00d
bnowel <bnowel@yahoo.com>
2012-03-07 19:53:26 -0800

    stuff moves

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ world.setIo(io);
 
 io.sockets.on('connection', function (socket) {
     // Update our list of players Add me
-    world.addPlayer({id: socket.id, px:0, py:0, dx:0, dy:0});
+    world.addPlayer({id: socket.id, px:25, py:25, dx:1, dy:0});
     world.init();
     
     socket.broadcast.emit('playerCount', {players: world.howManyPlayers()});
