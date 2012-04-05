0ae131cd7ced1d6302f02ccd52b9fc4afb666348
clintcparker <clintcparker@gmail.com>
2012-04-05 00:18:50 +0000

    more testing, some restructuring

---
+++ static/js/helper/helper.js
@@ -0,0 +1,31 @@
+var helper = {
+    getFirstArrayElementIndexByPrpertyValue : function(arrIn, prop, val){
+        var i = -1,
+            indexes = this.getIndexesByPrpertyValue(arrIn, prop, val);
+            
+        if (indexes.length > 0) {
+            i = indexes[0];    
+        }
+        
+        return i;
+    },
+    getIndexesByPrpertyValue :  function(arrIn, prop, val){
+        var len = arrIn.length,
+            indexes = [];
+        if (len!=0){
+            for (var i=0;i<len;i++){
+                var el = arrIn[i];
+                if (el.hasOwnProperty(prop) && el[prop] === val){
+                    indexes.push(i);
+                }
+            }
+        }
+        return indexes;    
+    }
+}
+
+if (typeof exports !== 'undefined') {
+    if (typeof module !== 'undefined' && module.exports) {
+      exports = module.exports = helper;
+    }
+}
--- static/js/world/world_test.js
+++ static/js/world/world_test.js
@@ -1,33 +1,21 @@
 var assert = require('assert')
-  ,world = require("../../static/js/world")
-  
-
-//    module.exports = {
-//        'test World starts with no players':function() {
-//            assert.equal(world.howManyPlayers(), 0);
-//        },
-//        'test World adding players': function() {
-//           world.addPlayer({});
-//           assert.equal(world.howManyPlayers(), 1);
-//        },
-//        'test not atomic tests': function() {
-//            assert.equal(world.howManyPlayers(), 1);
-//        }
-//     
-//    };
+  ,world = require("./world")
     
 if (typeof module !== "undefined" && module === require.main) {
     var suiteExports = {}; 
     var suite = function(a, fun){
         fun();  
+        module.exports = suiteExports;
+        require("asyncjs").test.testcase(module.exports).exec()
     }
     
     var test = function(txt, testfun){
-        suiteExports["test "+ txt] = testfun;
+        suiteExports["test: " + txt] = testfun;
     }
-    
+     
     
 }
+
 
 suite('World', function() {
     test('World starts with no players', function() {
@@ -40,10 +28,10 @@ suite('World', function() {
     test('not atomic tests', function() {
         assert.equal(world.howManyPlayers(), 1);
     });
+    
 });
 
 if (typeof module !== "undefined" && module === require.main) {
-    module.exports = suiteExports;
-    require("asyncjs").test.testcase(module.exports).exec()
+    
 }
 
